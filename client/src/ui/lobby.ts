04325460067803bf435cf27898b4697cb35d4156
testworkPF <dev@testworks.co.kr>
2026-03-28 00:37:23 +0900

    fix: client URL 하드코딩 HTTPS로 수정 (characterSelect, lobby)

--- client/src/ui/lobby.ts
+++ client/src/ui/lobby.ts
@@ -1,6 +1,6 @@
 // MapConfig is not used here, using MapInfo instead
 
-const API_URL = import.meta.env.VITE_SERVER_URL || 'http://namuten.duckdns.org:3000';
+const API_URL = import.meta.env.VITE_SERVER_URL || 'https://namuten.duckdns.org';
 
 interface MapInfo {
   id: number;
